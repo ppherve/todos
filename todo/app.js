6a31c231a5d9af53769455a7697c1c13dd1e4d6f
hervé <perezherve310gmail.com>
2015-12-11 17:00:53 +0100

    firstcommit

---
+++ todo/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	$("form").on("submit",function(event){
+		
+		event.preventDefault();
+
+			var toAdd = $("#note").val();
+
+		 	$("#liste").append('<li><input class="ck" type="checkbox">'+toAdd+"</li>");
+
+	});
+
+
+	
+	$("#liste").on("click",".ck",function(){
+
+		$(this).parent().toggleclass("fait");
+
+
+
+
+
+
+
+	});
+
+
+
+ });
+
+
+
+
+

04a2ce65a66a27846170b4564d03f018468a9a14
Maxime <maxime@minutepapillote.fr>
2015-12-21 15:49:48 +0100

    Révision du js

--- todo/js/app.js
+++ todo/js/app.js
@@ -3,15 +3,21 @@ $(document).ready(function(){
 	$("form").on("submit",function(event){
 		
 		event.preventDefault();
-
-			var toAdd = $("#note").val();
-		 	$("#liste").append('<li><input class="ck" type="checkbox">'+toAdd+"</li>");
+		// Indentation
+		var toAdd = $("#note").val();
+	 	$("#liste").append('<li><input class="ck" type="checkbox">'+toAdd+"</li>");
 
 	});
 	
 	$("#liste").on("click",".ck",function(){
 		$(this).parent().toggleclass("fait");
 	});
+
+	/*
+		Pour les actions, la logique est la même :
+		- ajouter un listener au bouton
+		- ajouter, enlever, modifier le compteur de la liste
+	*/
 
  });
 

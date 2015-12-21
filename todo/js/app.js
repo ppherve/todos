3d1f35b86d18a8ed9953ca9b05f37dda46ef386e
Maxime <maxime@minutepapillote.fr>
2015-12-21 15:48:28 +0100

    Révision du html

--- todo/js/app.js
+++ todo/js/app.js
@@ -5,26 +5,13 @@ $(document).ready(function(){
 		event.preventDefault();
 
 			var toAdd = $("#note").val();
-
 		 	$("#liste").append('<li><input class="ck" type="checkbox">'+toAdd+"</li>");
 
 	});
-
-
 	
 	$("#liste").on("click",".ck",function(){
-
 		$(this).parent().toggleclass("fait");
-
-
-
-
-
-
-
 	});
-
-
 
  });
 
